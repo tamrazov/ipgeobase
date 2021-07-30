b14e1e82e81104c5932a59ff475ebed4c110ee37
Alex <box-570@yandex.ru>
2021-07-30 16:14:03 +0300

    fix readme and lib

--- README.md
+++ README.md
@@ -22,7 +22,17 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+```ruby
+require "ipgeobase"
+
+ip_meta = Ipgeobase.lookup('8.8.8.8')
+
+ip_meta.city # Ashburn
+ip_meta.country # United States
+ip_meta.countryCode # US
+ip.lat # 39.03
+ip.lon # -77.5
+```
 
 ## Development
 
--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -1,8 +1,28 @@
 # frozen_string_literal: true
 
 require_relative "ipgeobase/version"
+require 'uri'
+require 'net/http'
+require 'happymapper'
+require "addressable/uri"
 
 module Ipgeobase
   class Error < StandardError; end
   # Your code goes here...
+  class IpData
+    include HappyMapper
+
+    tag 'query'
+    element :city, String, tag: 'city'
+    element :country, String, tag: 'country'
+    element :countryCode, String, tag: 'countryCode'
+    element :lat, Integer, tag: 'lat'
+    element :lon, Integer, tag: 'lon'
+  end
+
+  def self.lookup(ip)
+    uri = Addressable::URI.parse("http://ip-api.com/xml/#{ip}")
+    res = Net::HTTP.get(uri)
+    IpData.parse(res)
+  end
 end
